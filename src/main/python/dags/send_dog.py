53086ec787e84bfb152772f8dd41feff43f5e8af
Manuel Lázaro <megabright@gmail.com>
2025-09-15 18:29:46 +0200

    feat: run docker job and add composer

--- src/main/python/dags/send_dog.py
+++ src/main/python/dags/send_dog.py
@@ -1,45 +1,28 @@
-from airflow.decorators import dag, task
+from airflow import DAG
+from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime
 
-@dag(
-    start_date=None,
-    schedule="0 * * * *",
+def load_env_file(filepath):
+    env_dict = {}
+    with open(filepath) as f:
+        for line in f:
+            if line.strip() and not line.startswith("#"):
+                key, value = line.strip().split("=", 1)
+                env_dict[key] = value
+    return env_dict
+
+with DAG(
+    "enviar_imagen_whatsapp",
+    start_date=datetime(2023, 1, 1),
+    schedule="*/5 * * * *",  # Cada 5 minutos
     catchup=False,
-)
-def send_dog():
-
-    @task
-    def run_docker_container():
-        import docker
-        client = docker.from_env()
-        container = client.containers.run("doggy-app", detach=True)
-        container.wait()  # Wait for the container to finish
-        logs = container.logs().decode('utf-8')
-        container.remove()  # Clean up the container
-        return logs
+) as dag:
     
-    @task
-    def run_bash():
-        import subprocess
-        result = subprocess.run(["docker", "run", "--env-file", r"D:\Users\Manu\Documents\Datos\dev\doggy\.env", "doggy-app"], capture_output=True, text=True)
-        return result.stdout.strip()
-
-
-
-    log_output = run_bash()
-        
-dag = send_dog()
-
-if __name__ == "__main__":
-    conn_path = "connections.yaml"
-    variables_path = "variables.yaml"
-    my_conf_var = 23    
-
-    dag.test(
-        execution_date=datetime.now(),
-        conn_file_path=conn_path,
-        variable_file_path=variables_path,
-        run_conf={"my_conf_var": my_conf_var},
-        mark_success_pattern="dog.*",  # regex of task ids to be auto-marked as successful
-        use_executor=True
+    ejecutar_job = DockerOperator(
+        task_id="ejecutar_job",
+        image="doggy-app:latest",
+        auto_remove='force',
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",  # o la red de tus contenedores
+        environment=load_env_file("/opt/airflow/env/.env"),
     )
