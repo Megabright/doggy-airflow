9a7f0323058c3a64c19fc3cd71f94d9ad80003f3
Manuel Lázaro <megabright@gmail.com>
2025-09-17 11:55:48 +0200

    fix: dag and composer

--- src/main/python/dags/send_dog.py
+++ src/main/python/dags/send_dog.py
@@ -2,15 +2,6 @@ from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime
 
-def load_env_file(filepath):
-    env_dict = {}
-    with open(filepath) as f:
-        for line in f:
-            if line.strip() and not line.startswith("#"):
-                key, value = line.strip().split("=", 1)
-                env_dict[key] = value
-    return env_dict
-
 with DAG(
     "enviar_imagen_whatsapp",
     start_date=datetime(2023, 1, 1),
@@ -20,9 +11,8 @@ with DAG(
     
     ejecutar_job = DockerOperator(
         task_id="ejecutar_job",
-        image="doggy-app:latest",
+        image="doggy-producer:latest",
         auto_remove='force',
         docker_url="unix://var/run/docker.sock",
-        network_mode="bridge",  # o la red de tus contenedores
-        environment=load_env_file("/opt/airflow/env/.env"),
+        network_mode="doggy_doggy-net"
     )
